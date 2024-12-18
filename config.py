844b7a8a9ba0b314f1e133b7b9f7c2b363b24d93
alexgurliya <alexgurliya@gmail.com>
2024-12-18 19:34:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+api_id =23671274
+api_hash = "09b9c07a023f7c13c2164f2b22bd937e"
+bot_token = "8182324584:AAFy2bcf-IOZZvM6SkpSuXgxb3d4GsD_RM8"
+auth_users = "903077627"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = "903077627"
 owner_users = [int(num) for num in osowner_users.split(",")]
