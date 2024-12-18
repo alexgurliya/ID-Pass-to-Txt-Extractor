2835234e31d211e3246ec3c1577912b800207026
alexgurliya <alexgurliya@gmail.com>
2024-12-18 19:57:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_id =23671274
 api_hash = "09b9c07a023f7c13c2164f2b22bd937e"
-bot_token = "8182324584:AAFy2bcf-IOZZvM6SkpSuXgxb3d4GsD_RM8"
+bot_token = "AAFy2bcf-IOZZvM6SkpSuXgxb3d4GsD_RM8"
 auth_users = "903077627"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "903077627"
