a7ca81f88fb1a2b3c8a1fdbf923991495243c50e
alexgurliya <alexgurliya@gmail.com>
2025-01-02 16:28:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+import os
+
+api_id =  os.environ.get("API_ID", "23671274")
+api_hash = os.environ.get("API_HASH", "09b9c07a023f7c13c2164f2b22bd937e")
+bot_token = os.environ.get("BOT_TOKEN", " ")
+auth_users = os.environ.get("AUTH_USERS", "903077627,903077627")
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = os.environ.get("OWNER_USERS", "903077627,1369808729")
 owner_users = [int(num) for num in osowner_users.split(",")]
